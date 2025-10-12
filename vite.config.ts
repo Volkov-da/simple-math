bb06d17d55dc5a6f170c674ae461aef6b0eb7a3d
Volkov-da <dmitry.volkov@skoltech.ru>
2025-10-11 22:21:43 -0300

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Firebase Hosting serves from root, no base path needed
+  base: '/simple-math/', // GitHub Pages base path
 });
